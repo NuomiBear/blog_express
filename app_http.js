2b1615078ef46d6a6533ff4935be5e0e954a841a
NuomiBear <niasaimoe@gmail.com>
2024-01-03 15:42:54 +0800

    blog_express

---
+++ app_http.js
@@ -0,0 +1,19 @@
+// var http = require('http')
+
+// var app = http.createServer((req, res) => {
+//     res.writeHead(200, { "ContentType": "text/plain" })
+//     res.end("hello")
+// })
+
+// app.listen(3000, "localhost")
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send("express")
+// })
+
+// app.listen(3000, () => {
+//     console.log("express")
+// })
